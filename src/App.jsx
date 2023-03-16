e14d7b0f964eb965ec09b304c3edcc6fc7996fc5
busayolawumi <busayoolawumi3@gmail.com>
2023-03-16 14:40:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import hotBg from './assets/hot.jpg';
+import coldBg from './assets/cold.jpg';
+import Descriptions from './Components/Descriptions';
+
+const App = () => {
+  return (
+    <>
+      <div className='app' style={{backgroundImage: `url(${coldBg})`}}>
+       <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City..." />
+            <button>°F</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>90°C</h1>
+            </div>
+          </div>
+
+          {/* bottom description */}
+          <Descriptions />
+        </div>
+       </div>
+      </div>
+    </>
+  )
+}
+
+export default App
